23c6c0ee08611651b0d935c60b8451bd4319ad11
Dan Gutt <dgutt10@gmail.com>
2019-03-15 08:31:31 -0500

    made a successful get request through the app to the neo4j db

---
+++ server/api/movie.js
@@ -0,0 +1,14 @@
+const router = require('express').Router()
+const {session} = require('../db/neo')
+module.exports = router
+
+router.get('/', async (req, res, next) => {
+  try {
+    let query = `MATCH (n) return n`
+    let result = await session.run(query)
+    console.log('result: ', result)
+    res.json(result)
+  } catch (err) {
+    next(err)
+  }
+})
--- server/db/neo.js
+++ server/db/neo.js
@@ -1,8 +1,10 @@
 const neo4j = require('neo4j-driver').v1
+const dotenv = require('dotenv')
+dotenv.config()
 
-const graphURI = process.env.GRAPH_BOLT_URI || 'bolt://localhost'
-const graphUsername = process.env.GRAPH_BOLT_USER || 'neo4j'
-const graphPassword = process.env.GRAPH_BOLT_PASSWORD || '123456'
+const graphURI = process.env.GRAPH_BOLT_URI
+const graphUsername = process.env.GRAPH_BOLT_USER
+const graphPassword = process.env.GRAPH_BOLT_PASSWORD
 
 const driver = neo4j.driver(
   graphURI,
