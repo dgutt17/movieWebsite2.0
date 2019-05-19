bf7921879b207e57e2480dff6a9385e20760a25a
Dan Gutt <dgutt10@gmail.com>
2019-05-19 16:04:56 -0500

    small change to the server code

--- server/api/movie.js
+++ server/api/movie.js
@@ -6,8 +6,8 @@ router.get('/', async (req, res, next) => {
   try {
     let query = `MATCH (n) return n`
     let result = await session.run(query)
-    console.log('result: ', result)
-    res.json(result)
+    console.log('result: ', result.records)
+    res.json(result.records)
   } catch (err) {
     next(err)
   }
